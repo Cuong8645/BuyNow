83e8bda6b7c8b0dd6eaa6f8083e240cca4c51603
ACER <colorf8645@gmail.com>
2019-04-03 09:07:25 +0700

    test

---
+++ app/src/main/java/com/example/buynow/Sua_san_pham.java
@@ -0,0 +1,13 @@
+package com.example.buynow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sua_san_pham extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sua_san_pham_activity);
+    }
+}
