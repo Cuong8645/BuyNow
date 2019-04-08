a92e4283ca5b18100d844b1ecddf1f2e9b648a81
NguyenHai1449 <nguyenhai1449@gmail.com>
2019-04-08 10:35:28 +0700

    Thiet ke giao dien man hinh cai dat thong tin, loc ket qua, tao cua hang, by nguyen van hai

---
+++ app/src/main/java/com/example/buynow/CaiDatThongTinActivity.java
@@ -0,0 +1,13 @@
+package com.example.buynow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CaiDatThongTinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cai_dat_thong_tin_layout);
+    }
+}
---
+++ app/src/main/java/com/example/buynow/TaoCuaHangActivity.java
@@ -0,0 +1,13 @@
+package com.example.buynow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaoCuaHangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tao_cua_hang_layout);
+    }
+}
