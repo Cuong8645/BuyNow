4ae5523f2035a2a74750f213f46650629801c2b9
ACER <colorf8645@gmail.com>
2019-04-08 08:55:53 +0700

    hghjghjdsffsd

--- app/src/main/java/com/example/buynow/Box_chat.java
+++ app/src/main/java/com/example/buynow/Box_chat.java
@@ -4,15 +4,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Sua_san_pham extends AppCompatActivity {
+public class Box_chat extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sua_san_pham_activity);
+        setContentView(R.layout.box_chat_activity);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("Sửa sản phẩm"); //Thiết lập tiêu đề nếu muốn
+        actionBar.setTitle("Box Chat"); //Thiết lập tiêu đề nếu muốn
         String title = actionBar.getTitle().toString(); //Lấy tiêu đề nếu muốn
     }
 }
--- app/src/main/java/com/example/buynow/Dang_san_pham.java
+++ app/src/main/java/com/example/buynow/Dang_san_pham.java
@@ -1,10 +1,14 @@
 package com.example.buynow;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Dang_san_pham extends AppCompatActivity {
 
@@ -13,6 +17,16 @@ public class Dang_san_pham extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dang_san_pham_activity);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Đăng sản phẩm"); //Thiết lập tiêu đề nếu muốn
+        String title = actionBar.getTitle().toString(); //Lấy tiêu đề nếu muốn
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
 
     }
     @Override
